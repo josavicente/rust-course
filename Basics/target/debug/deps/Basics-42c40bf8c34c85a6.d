76363c106df749449cc7b2b1bcc94c3e0c319ba0
Josa Vicente <jvicenpe@gmail.com>
2022-11-24 13:43:03 +0100

    Basiscs :construction:

---
+++ Basics/target/debug/deps/Basics-42c40bf8c34c85a6.d
@@ -0,0 +1,5 @@
+/Users/josa/Documents/Developer/Learning/Rust/Basics/target/debug/deps/Basics-42c40bf8c34c85a6.rmeta: src/main.rs
+
+/Users/josa/Documents/Developer/Learning/Rust/Basics/target/debug/deps/Basics-42c40bf8c34c85a6.d: src/main.rs
+
+src/main.rs:
---
+++ Basics/target/debug/deps/Basics-9d6d4b788f7024a8.d
@@ -0,0 +1,5 @@
+/Users/josa/Documents/Developer/Learning/Rust/Basics/target/debug/deps/Basics-9d6d4b788f7024a8.rmeta: src/main.rs
+
+/Users/josa/Documents/Developer/Learning/Rust/Basics/target/debug/deps/Basics-9d6d4b788f7024a8.d: src/main.rs
+
+src/main.rs:
